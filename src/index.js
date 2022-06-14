29bfd8a6d82bad434dbd777fe4da0667957bafff
Temik-hub <surshkov.art@gmail.com>
2022-06-14 22:21:43 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/main.scss'
+import './js/slider-1.js'
+import './js/board.js'
+import './js/slider-2.js'
---
+++ src/js/slider-1.js
@@ -0,0 +1,19 @@
+function slidesPlagin(activeSlide = 0) {
+  const slides = document.querySelectorAll('.slider__item')
+  
+  slides[activeSlide].classList.add('slider__item--active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('slider__item--active')
+    })
+  }
+  
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('slider__item--active')
+    })
+  }
+}
+slidesPlagin(1)
